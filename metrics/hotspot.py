d5a1e8f04ed57d0a1ec891827435a24b8dc5b005
stbraun <sb@action.ms>
2015-05-03 17:29:30 +0200

    Initial version. Runs hot-spot analysis and plots #loc vs. #revisions. Template and d3.js lib added for creating enclosure diagrams. howto.org added.

---
+++ metrics/hotspot.py
@@ -0,0 +1,77 @@
+# coding=utf-8
+"""
+Copyright (c) 2015 Stefan Braun
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and
+associated documentation files (the "Software"), to deal in the Software without restriction,
+including without limitation the rights to use, copy, modify, merge, publish, distribute,
+sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or
+substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
+INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE
+AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+"""
+
+import pandas as pd
+import matplotlib.pyplot as plt
+
+
+class InvalidDataException(Exception):
+    """Data validation failed."""
+    pass
+
+
+class HotspotAnalyzer(object):
+    """Perform a hot-spot analysis on a data set.
+
+    The data set is expected as path to a csv file with - at least - following columns:
+    * module - the file
+    * revisions - number of revisions
+    * code - number of code lines
+    """
+
+    def __init__(self, data_file_path: str):
+        """Initialize the analyzer with the given data set.
+
+        Parameters:
+
+        :param data_file_path: path to csv file [module,revisions,code].
+        """
+        super().__init__()
+        self.expected_columns = {"module", "revisions", "code"}
+        self.path = data_file_path
+        self.dta = self.__read_csv()
+        self.__validate_data()
+
+    def __read_csv(self) -> pd.DataFrame:
+        """Read csv file data.
+            Convert column headers and table names to upper case
+            supporting both Oracle and MSSql-Server.
+
+            :return: data frame.
+            :rtype: pd.DataFrame
+            """
+        dta = pd.read_csv(self.path)
+        return dta
+
+    def __validate_data(self):
+        """Validate required columns.
+        :raise InvalidDataException: in case of missing columns.
+        """
+        if not self.expected_columns.issubset(self.dta.columns):
+            raise InvalidDataException("Required columns missing: {}".format(self.expected_columns))
+
+    def plot_revisions_vs_code(self):
+        """Plot data."""
+        fig = plt.figure(figsize=(15, 8))
+        fig_line = fig.add_subplot(2, 1, 1)
+        cdf = self.dta
+        cdf.plot(kind='scatter', x='code', y='revisions', title='revisions over lines of code', color='b', ax=fig_line)
+        plt.show()
+        plt.close()
---
+++ setup.py
@@ -0,0 +1,47 @@
+import sys
+from setuptools import setup, find_packages
+from setuptools.command.test import test as TestCommand
+
+
+version = '0.1.0dev1'
+
+
+class PyTest(TestCommand):
+    """This is a plug-in for setuptools.
+
+     It will invoke py.test when you run python setup.py test
+    """
+    def finalize_options(self):
+        """Configure."""
+        TestCommand.finalize_options(self)
+        self.test_args = []
+        self.test_suite = True
+
+    def run_tests(self):
+        """Execute tests."""
+        import pytest  # import here, because outside the required eggs aren't loaded yet
+        sys.exit(pytest.main(self.test_args))
+
+
+setup(
+    name='metrics',
+    version=version,
+    description='Simple code metrics',
+    long_description=open("README.rst").read(),
+    package_dir={'': '.'},
+    url='http://xx.yy.zzz', # TODO
+    license='MIT',
+    keywords='development tools',  # Separate with spaces
+    author='Stefan Braun',
+    author_email='sb@action.ms',
+    packages=find_packages(exclude=['examples', 'tests']),
+    include_package_data=True,
+    zip_safe=False,
+    cmdclass={'test': PyTest},
+    scripts=['runHotspotAnalysis.py', ],
+
+    # List of packages that this one depends upon:
+    install_requires=['argh', 'setuptools'],
+    requires=['argh', 'setuptools'],
+    provides=['metrics']
+)
